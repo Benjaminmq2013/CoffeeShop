83f23a4cc4f47dd7b17afa5679c89d1b6dd441d7
Adriel Martinez <benjaminmq2013@gmail.com>
2020-11-06 20:29:34 -0600

    Terminando responsive 70%

---
+++ anim.js
@@ -0,0 +1,22 @@
+const lateral_menu = document.querySelector(".header_container")
+const botoncito = document.getElementById("btn_menu")
+
+/* PODEMOS TRAER ELEMENTOS POR "ID" O "querySelector (clase)" */
+
+
+botoncito.addEventListener("click", leftRight);
+
+
+function leftRight(){
+    if (lateral_menu.classList.contains("menu-appear")){
+
+        lateral_menu.classList.remove("menu-appear")
+        lateral_menu.classList.add("menu-disappear")
+    } else{
+        lateral_menu.classList.remove("menu-disappear")
+        lateral_menu.classList.add("menu-appear")
+    }
+    
+
+
+}
