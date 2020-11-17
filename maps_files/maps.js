9aea8f4ee530add52b1d8ffe6f47804d121c01f8
Adriel Martinez <benjaminmq2013@gmail.com>
2020-11-17 12:23:03 -0600

    Drop down menu finished

--- maps_files/maps.js
+++ maps_files/maps.js
@@ -1,17 +1,16 @@
-// Initialize and add the map
+
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
-    // The map, centered at Uluru
+
+    const uluru = { lat: 13.4833, lng: -88.18333 };
+    
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
+      zoom: 10,
       center: uluru,
     });
-    // The marker, positioned at Uluru
+    
     const marker = new google.maps.Marker({
       position: uluru,
       map: map,
     });
   }
 
-  console.log("Hola putas")
